f94341b348f8d53e8dd17f7f30ceb3201a09b707
Vasco Oliveira <vascoliveira2511@gmail.com>
2024-09-30 10:42:42 +0100

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
 import IssuesStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
 
@@ -26,7 +26,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssuesStatusBadge status={issue.status} />
                 </div>
