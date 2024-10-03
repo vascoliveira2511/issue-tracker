d957378879038c11edbeeb51d768ef8a7a9ced57
Vasco Oliveira <vascoliveira2511@gmail.com>
2024-10-03 09:34:34 +0100

    Add drop down for current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -45,10 +52,40 @@ const NavBar = () => {
             {" "}
             <Box>
               {status === "authenticated" && (
-                <Link href="/api/auth/signout">Log out</Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Avatar
+                      src={session.user!.image!}
+                      fallback={"?"}
+                      size={"2"}
+                      radius="full"
+                      className="cursor-pointer"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size="2">{session.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href="/api/auth/signout">Logout</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
               )}
               {status === "unauthenticated" && (
-                <Link href="/api/auth/signin">Login</Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <button className="text-zinc-900 hover:text-zinc-800 transition-colors">
+                      Login
+                    </button>
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content sideOffset={5}>
+                    <DropdownMenu.Item>
+                      <Link href="/api/auth/signin">Login</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
+                //<Link href="/api/auth/signin">Login</Link>
               )}
             </Box>
           </Box>
