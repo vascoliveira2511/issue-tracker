6d2ee1bdacb17ea6b5ba415afab74c5049ba8948
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2024-10-09 23:02:08 +0100

    Build the filter comp

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import React from "react";
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "Closed", value: "CLOSED" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+];
+
+const IssueStatusFilter = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Filter by status..."></Select.Trigger>
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.label} value={status.value || "All"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
