1f9919186cc2820f6e7adab6aa736934efa5d276
Vasco Mota de Oliveira <vascoliveira2511@gmail.com>
2024-10-10 22:03:39 +0100

    Latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: {
-    page: string;
-  };
-}) {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page</p>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </div>
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
